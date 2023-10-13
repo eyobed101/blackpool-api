181e5b4e3361490f3bcd1ae9002e1df712a6ec50
Eyobed Elias <eyobed@insa.gov.et>
2023-10-13 16:55:39 +0300

    relation

--- app/Models/Verification.php
+++ app/Models/Verification.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Verification extends Model
 {
     use HasFactory;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
 
 
