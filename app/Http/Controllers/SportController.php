015e323ba86639dfcc306e3afdc3ad0c7b54e335
Eyobed Elias <eyobed@insa.gov.et>
2023-10-16 17:17:42 +0300

    return json

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -2,7 +2,7 @@
 
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Cache;
 use GuzzleHttp\Client;
 
 
@@ -14,7 +14,7 @@ class SportController extends Controller
     public function __construct()
     {
         $this->client = new Client([
-            'base_uri' => 'https://api.the-odds-api.com/V4/sport', 
+            'base_uri' => 'https://api.the-odds-api.com', 
             'headers' => [
                 'Authorization' => '8b0b6949dd4456a8534cd76543bc3c7e', 
                 'Accept' => 'application/json',
@@ -24,21 +24,25 @@ class SportController extends Controller
 
     public function getGames()
     {
-        $games = Cache::remember('recent_odds', 60, function () {
-            $response = $this->client->get('/odds'); 
+        $games = Cache::remember('recent_odds', 3600, function () {
+            $response = $this->client->get('/V4/sport/upcoming/odds'); 
             return json_decode($response->getBody(), true);
         });
 
-        return view('games', compact('games'));
+        // return view('games', compact('games'));
+        return response()->json($games);
+
     }
 
     public function getScores()
     {
         $scores = Cache::remember('recent_scores', 60, function () {
-            $response = $this->client->get('/scores'); 
+            $response = $this->client->get('/V4/sport/scores'); 
             return json_decode($response->getBody(), true);
         });
 
-        return view('scores', compact('scores'));
+        // return view('scores', compact('scores'));
+        return response()->json($scores);
+
     }
 }
